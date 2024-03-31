619eaac788fa435552574668800d939b1af7564e
psujit11 <105852990+psujit11@users.noreply.github.com>
2024-03-31 19:21:21 +0545

    LeadDtoscreated

--- CRM.API/obj/Debug/net8.0/CRM.API.AssemblyInfo.cs
+++ CRM.API/obj/Debug/net8.0/CRM.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+310cfe655109025b0d21f1715bfff9a62697ff1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3b694f6f69ac7cecc26407245a15f52c2fc082")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
