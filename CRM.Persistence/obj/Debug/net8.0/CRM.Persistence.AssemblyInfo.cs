488bb4d501b6ed4f8909019d7ed0b1699fa44ebd
mshirish <104812313+mshirish@users.noreply.github.com>
2024-04-04 12:00:37 +0545

    Test commit

--- CRM.Persistence/obj/Debug/net8.0/CRM.Persistence.AssemblyInfo.cs
+++ CRM.Persistence/obj/Debug/net8.0/CRM.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6a861c16b830766f112d5f04ed6284f2b037a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f715505057f57c58baaa315a39703f586f00a06")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
